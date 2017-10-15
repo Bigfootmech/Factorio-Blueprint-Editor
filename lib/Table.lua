c1b5d9346bbae635db2ab844120daa367a49f714
Bigfootmech <bigfootmech@gmail.com>
2017-10-15 01:40:30 +0100

    0.0.1 Table functions

---
+++ lib/Table.lua
@@ -0,0 +1,23 @@
+Table = {}
+Table.__index = Table
+
+function Table.insert_all(this, other)
+    assert(type(this) == "table", "Cannot insert all to a non-table.")
+    assert(type(other) == "table", "Cannot insert all a single value.")
+    
+    for k,v in pairs(other) do this[k] = v end
+    return this
+end
+
+function Table:deepcopy(source)
+    if type(source) ~= "table" then return source end
+    assert(type(source) == "table", "Cannot copy a non-table as a table.")
+    local copy = {}
+    
+    for k, v in next, source, nil do
+        copy[Table:deepcopy(k)] = Table:deepcopy(v)
+    end
+    setmetatable(copy, Table:deepcopy(getmetatable(source)))
+    
+    return copy
+end
