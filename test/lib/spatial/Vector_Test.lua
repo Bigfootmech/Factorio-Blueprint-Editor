18fd04f5a6824a1873d3fd520db4497cecc6029a
Bigfootmech <bigfootmech@gmail.com>
2017-10-15 23:27:12 +0100

    0.0.1 rewrote vector test in lua unit, and added a couple more

--- test/lib/spatial/Vector_Test.lua
+++ test/lib/spatial/Vector_Test.lua
@@ -1,11 +1,67 @@
 
--- test bed?
--- vec = Vector:new(0, 1) -- Create a vector
--- print(vec:is_instatiated())     -- Is vector instantiated? (output: true)
--- print(vec:get_magnitude())          -- Call a method (output: 1)
--- print(vec[1])                    -- Access a member variable (output: 0)
--- print(vec[2])                    -- Access a member variable (output: 1)
--- print(Vector:is_vector(vec)) -- true
--- print(Vector:is_vector({1,2})) -- true
--- print(Vector:is_vector({1})) -- false
--- print(Vector:is_vector({1,2,3})) -- true
+local lu = require('luaunit')
+local Vector = require('lib.spatial.Vector')
+
+TestVectorCreated = {}
+    function TestVectorCreated:testCreated()
+        -- given
+        local x = 0
+        local y = 1
+        -- when
+        vec = Vector.new(x, y)
+        -- then
+        lu.assertEquals(vec[1], 0)
+        lu.assertTrue(vec[2], 1)
+    end
+    
+    function TestVectorCreated:testCreatedIndependently()
+        -- given
+        local x1 = 0
+        local y1 = 1
+        local x2 = 2
+        local y2 = 3
+        -- when
+        vec1 = Vector.new(x1, y1)
+        vec2 = Vector.new(x2, y2)
+        -- then
+        lu.assertEquals(vec1[1], 0)
+        lu.assertTrue(vec1[2], 1)
+        lu.assertEquals(vec2[1], 2)
+        lu.assertTrue(vec2[2], 3)
+    end
+    
+    function TestVectorCreated:testTooShortFails()
+        -- given
+        local x = 1
+        -- when -- then
+        lu.assertError(Vector.new, x)
+    end
+    
+TestIsVectorWorks = {}
+    function TestIsVectorWorks:testNormalVectorWorks()
+        -- given
+        local x = 1
+        local y = 2
+        -- when
+        vec = Vector.new(x, y)
+        -- then
+        lu.assertTrue(Vector.is_vector(vec))
+    end
+    
+    function TestIsVectorWorks:testTooLongWorks()
+        -- given
+        local x = 1
+        local y = 2
+        local random = 3
+        -- when
+        vec = Vector.new(x, y,random)
+        -- then
+        lu.assertTrue(Vector.is_vector(vec))
+        lu.assertTrue(Vector.is_vector({x,y,random}))
+    end
+    
+    function TestIsVectorWorks:testTooShortFails()
+        lu.assertFalse(Vector.is_vector({1}))
+    end
+ 
+os.exit(lu.LuaUnit.run())
