d3a5e7fd64d0e5850d892579a43c8450db3a9224
Bigfootmech <bigfootmech@gmail.com>
2017-10-19 16:41:26 +0100

    0.0.3 command_unification made it so that action definitions are created on method call, rather than import. Also, removed clutter code. Need to rename Event_Registration.lua

--- bpedit/keybinds/Actions.lua
+++ bpedit/keybinds/Actions.lua
@@ -5,41 +5,51 @@ local Event_Registration = require('lib.events.Event_Registration')
 
 local Actions = {}
 
-Event_Registration.register_command_from_action_definition({
-    [Util.action_name_field_name] = "Primary Action", 
-    [Util.locale_text_field_name] = "Edit/Reopen", 
-    [Util.key_sequence_field_name] = "N", 
-    [Util.linked_function_field_name] = "edit_or_reopen_blueprint"})
+local function get_ordered_action_definitions()
     
-Event_Registration.register_command_from_action_definition({
-    [Util.action_name_field_name] = "Secondary Action", 
-    [Util.locale_text_field_name] = "Add Component", 
-    [Util.key_sequence_field_name] = "SHIFT + N", 
-    [Util.linked_function_field_name] = "add_inner_blueprint"})
+    local ordered_action_definitions = {}
+    
+    table.insert(ordered_action_definitions,{
+        [Util.action_name_field_name] = "Primary Action", 
+        [Util.locale_text_field_name] = "Edit/Reopen", 
+        [Util.key_sequence_field_name] = "N", 
+        [Util.linked_function_field_name] = "edit_or_reopen_blueprint"
+        })
+        
+    table.insert(ordered_action_definitions, {
+        [Util.action_name_field_name] = "Secondary Action", 
+        [Util.locale_text_field_name] = "Add Component", 
+        [Util.key_sequence_field_name] = "SHIFT + N", 
+        [Util.linked_function_field_name] = "add_inner_blueprint"
+        })
+    
+    local function get_filled_direction_action_definition(direction_name)
+        local uncooked_definition = Direction_Keys.get_action_definition(direction_name)
+        uncooked_definition[Util.linked_function_field_name] = "move_inner_blueprint"
+        uncooked_definition[Util.locale_text_field_name] = "Selected Move " .. uncooked_definition[Util.locale_text_field_name]
+        uncooked_definition[Util.var_field_name] = uncooked_definition[Util.var_field_name]:divide(2)
+    end
+    
+    local function get_filled_enhanced_direction_action_definition(direction_name)
+        local uncooked_definition = Direction_Keys.get_action_definition(direction_name)
+        uncooked_definition[Util.action_name_field_name] = uncooked_definition[Util.action_name_field_name] .. "More"
+        uncooked_definition[Util.locale_text_field_name] = "Selected Move Further " .. uncooked_definition[Util.locale_text_field_name]
+        uncooked_definition[Util.key_sequence_field_name] = "SHIFT + ".. uncooked_definition[Util.key_sequence_field_name]
+        uncooked_definition[Util.linked_function_field_name] = "move_inner_blueprint"
+        uncooked_definition[Util.var_field_name] = uncooked_definition[Util.var_field_name]:multiply(2)
+    end
+    
+    for direction_name, _ in pairs(Direction_Keys.names) do
+        table.insert(ordered_action_definitions,get_filled_direction_action_definition(direction_name))
+        table.insert(ordered_action_definitions,get_filled_enhanced_direction_action_definition(direction_name))
+    end
 
-local function get_filled_direction_action_definition(direction_name)
-    local uncooked_definition = Direction_Keys.get_action_definition(direction_name)
-    uncooked_definition[Util.linked_function_field_name] = "move_inner_blueprint"
-    uncooked_definition[Util.locale_text_field_name] = "Selected Move " .. uncooked_definition[Util.locale_text_field_name]
-    uncooked_definition[Util.var_field_name] = uncooked_definition[Util.var_field_name]:divide(2)
+    return ordered_action_definitions
 end
 
-local function get_filled_enhanced_direction_action_definition(direction_name)
-    local uncooked_definition = Direction_Keys.get_action_definition(direction_name)
-    uncooked_definition[Util.action_name_field_name] = uncooked_definition[Util.action_name_field_name] .. "More"
-    uncooked_definition[Util.locale_text_field_name] = "Selected Move Further " .. uncooked_definition[Util.locale_text_field_name]
-    uncooked_definition[Util.key_sequence_field_name] = "SHIFT + ".. uncooked_definition[Util.key_sequence_field_name]
-    uncooked_definition[Util.linked_function_field_name] = "move_inner_blueprint"
-    uncooked_definition[Util.var_field_name] = uncooked_definition[Util.var_field_name]:multiply(2)
-end
+Actions.get_interface_mapping = function() return Event_Registration.get_interface_mapping(get_ordered_action_definitions()) end
+Actions.get_var_for_event = function(event_name) return Event_Registration.get_var_for_event(event_name, get_ordered_action_definitions()) end
 
-for direction_name, _ in pairs(Direction_Keys.names) do
-    Event_Registration.register_command_from_action_definition(get_filled_direction_action_definition(direction_name))
-    Event_Registration.register_command_from_action_definition(get_filled_enhanced_direction_action_definition(direction_name))
-end
-
-Actions.get_interface_mapping = Event_Registration.get_interface_mapping
-Actions.get_registered_key_sequences = Event_Registration.get_registered_key_sequences
-Actions.get_var_for_event = Event_Registration.get_var_for_event
+Actions.get_registered_key_sequences = function() return Event_Registration.get_registered_key_sequences(get_ordered_action_definitions()) end
 
 return Actions
--- lib/events/Event_Registration.lua
+++ lib/events/Event_Registration.lua
@@ -1,87 +1,58 @@
-local Util = require 'lib.events.Util'
-local Table = require 'lib.lua_enhance.Table'
+local Util = require('lib.events.Util')
+local Table = require('lib.lua_enhance.Table')
 
 local Event_Registration = {}
 
-local registered_events_list ={}
-local function add_event_to_registered(event_name)
-    table.insert(registered_events_list, event_name)
+local function get_locale_text(ordered_action_definitions)
+    local locale_text ={}
+    
+    for i,action_definition in ipairs(ordered_action_definitions) do
+        local event_name = Util.get_event_name(action_definition[Util.action_name_field_name], i)
+        locale_text[event_name] = action_definition[Util.locale_text_field_name]
+        return true
+    end
+    
+    return locale_text
 end
-local function get_registered_events_list()
-    return Table.deepcopy(registered_events_list)
-end
-Event_Registration.get_registered_events_list = get_registered_events_list
+Event_Registration.get_locale_text = get_locale_text -- USE TO GENERATE locale/en/controls.cfg
 
-local registered_locale_text ={}
-local function register_locale_text(event_name, locale_text)
-    registered_locale_text[event_name] = locale_text
-end
-local function get_locale_text_for_event(event_name)
-    return registered_locale_text[event_name]
-end
-Event_Registration.get_locale_text_for_event = get_locale_text_for_event -- USE TO GENERATE locale/en/controls.cfg
-
-local registered_key_sequences ={}
-local function register_key_sequence(event_name, key_sequence)
-    table.insert(registered_key_sequences, Util.get_prototype_table(event_name, key_sequence))
-end
-local function get_registered_key_sequences(event_name)
-    return Table.deepcopy(registered_key_sequences)
+local function get_registered_key_sequences(ordered_action_definitions)
+    local registered_key_sequences ={}
+    
+    for i,action_definition in ipairs(ordered_action_definitions) do
+        local event_name = Util.get_event_name(action_definition[Util.action_name_field_name], i)
+        table.insert(registered_key_sequences, Util.get_prototype_table(event_name, action_definition[Util.key_sequence_field_name]))
+    end
+    
+    return registered_key_sequences
 end
 Event_Registration.get_registered_key_sequences = get_registered_key_sequences
 
-local registered_var ={}
-local function register_var(event_name, var)
-    registered_var[event_name] = var
-end
-local function get_var_for_event(event_name)
-    return registered_var[event_name]
+local function get_var_for_event(event_name_to_match, ordered_action_definitions)
+    for i,action_definition in ipairs(ordered_action_definitions) do
+        local event_name = Util.get_event_name(action_definition[Util.action_name_field_name], i)
+        if(event_name_to_match == event_name) then
+            return action_definition[Util.var_field_name]
+        end
+    end
+    return nil -- error?
 end
 Event_Registration.get_var_for_event = get_var_for_event
 
-local registered_api_for_event = {}
-local registered_events_for_api = {}
-local function register_event_to_api(event_name, api)
-    if type(registered_events_for_api[api]) ~= "table" then registered_events_for_api[api] = {} end
-    return table.insert(registered_events_for_api[api], event_name)
-end
-local function register_api(event_name, api)
-    registered_api_for_event[event_name] = api
-    register_event_to_api(event_name, api)
-end
-local function get_api_for_event(event_name)
-    return registered_var[event_name]
-end
-Event_Registration.get_api_for_event = get_api_for_event
-local function get_api_register_list()
-    return Table.deepcopy(registered_events_for_api)
+local function get_api_register_list(ordered_action_definitions)
+    local registered_events_for_api = {}
+    
+    for i,action_definition in ipairs(ordered_action_definitions) do
+        local event_name = Util.get_event_name(action_definition[Util.action_name_field_name], i)
+        local api_name = action_definition[Util.linked_function_field_name]
+        
+        registered_events_for_api[api_name] = registered_events_for_api[api_name] or {}
+        -- if type(registered_events_for_api[api_name]) ~= "table" then registered_events_for_api[api_name] = {} end
+        table.insert(registered_events_for_api[api_name], event_name)
+    end
+    
+    return registered_events_for_api
 end
 Event_Registration.get_interface_mapping = get_api_register_list
-
-----------------------------------------------------------------
-
-local function register_command(action_name, locale_text, key_sequence , linked_function, var)
-    local new_event_number = #registered_events_list + 1
-    local event_name = Util.get_event_name(action_name, new_event_number)
-    register_locale_text(event_name, locale_text)
-    register_key_sequence(event_name, key_sequence)
-    register_api(event_name, linked_function)
-    if var ~= nil then
-        register_var(event_name, var)
-    end
-    add_event_to_registered(event_name) -- do I need to preserve action_name here??
-    return true
-end
-Event_Registration.register_command = register_command
-
-local function register_command_from_action_definition(action_definition)
-    return register_command(
-    action_definition[Util.action_name_field_name], 
-    action_definition[Util.locale_text_field_name], 
-    action_definition[Util.key_sequence_field_name], 
-    action_definition[Util.linked_function_field_name], 
-    action_definition[Util.var_field_name])
-end
-Event_Registration.register_command_from_action_definition = register_command_from_action_definition
 
 return Event_Registration
