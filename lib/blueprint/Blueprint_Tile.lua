3495edbb6ee5bcba8f78064ac232788617d745f6
Bigfootmech <bigfootmech@gmail.com>
2017-10-21 09:31:40 +0100

    0.0.3 new classes. Not sure if I should do anything with them yet

---
+++ lib/blueprint/Blueprint_Tile.lua
@@ -0,0 +1,13 @@
+--[[
+get_blueprint_tiles() → array of blueprint tile
+
+Tiles in this blueprint
+
+Return value
+A blueprint tile is a table:
+
+    position :: Position
+    name :: string: Prototype name of the tile.
+
+Can only be used if this is BlueprintItem
+]]
--- lib/icons/Signal_Id.lua
+++ lib/icons/Signal_Id.lua
@@ -7,3 +7,24 @@ Table with the following fields:
     type :: string: "item", "fluid", or "virtual".
     name :: string (optional): Name of the item, fluid or virtual signal.
 ]]
+local Signal_Id = {}
+Signal_Id.__index = Signal_Id
+
+function Signal_Id:is_signal_id()
+    return self ~= nil and type(self) == "table" and self.type ~= nil
+end
+
+local function new(signal_type, signal_name)
+    assert(type(signal_type) == "string", "signal_type must be a string")
+    if(signal_name ~= nil) then
+        assert(type(signal_name) == "string", "signal_name must be a string, if it exists.")
+    end
+    return setmetatable({type = signal_type, name = signal_name}, Signal_Id)
+end
+Signal_Id.new = new
+
+function Signal_Id:copy()
+    return new(self.type, self.name)
+end
+
+return Signal_Id
