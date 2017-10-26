2cbab37f0845c8cbf2f50bce84c6ed3b849ea956
Bigfootmech <bigfootmech@gmail.com>
2017-10-26 21:04:11 +0100

    0.0.3 Reworking how "API" retrieves / checks for information. Simplifying, and error checking player.

--- bpedit/frontend/Api.lua
+++ bpedit/frontend/Api.lua
@@ -10,14 +10,35 @@ local function get_player_store(player)
     return Global_Dao.get_player_store(player.index)
 end
 
+local function is_editing(player)
+    return get_player_store(player):is_editing()
+end
+
+local function has_blueprint_selection(player)
+    return get_player_store(player):has_selection()
+end
+
+local function has_mouseover_selection(player)
+    return player:get_selected()
+end
+
+local function has_item_gui_open(player)
+    return player:get_open_gui_type() == defines.gui_type.item
+end
+
+local function has_blueprint_in_hand(player)
+    if(player:get_blueprint_from_hand()) then return true end
+    return false
+end
+
 local function edit_or_reopen_blueprint(event)
     local player = Player.from_event(event)
     
-    if player:has_blueprint_in_hand() then
+    if has_blueprint_in_hand(player) then
         return Blueprint_Edit_Actions.begin_editing_blueprint(player)
     end
     
-    if get_player_store(player):is_editing() then
+    if is_editing(player) then
         return Blueprint_Edit_Actions.reopen_blueprint_menu(player)
     end
     
@@ -28,12 +49,12 @@ Api.edit_or_reopen_blueprint = edit_or_reopen_blueprint
 local function add_inner_blueprint(event)
     local player = Player.from_event(event)
     
-    if not get_player_store(player):is_editing() then
+    if not is_editing(player) then
         player:sendmessage("Can't add bp, not currently editing.")
         return false
     end
     
-    if not player:has_blueprint_in_hand() then
+    if not has_blueprint_in_hand(player) then
         player:sendmessage("No blueprint in hand to add.")
         return false
     end
@@ -45,12 +66,12 @@ Api.add_inner_blueprint = add_inner_blueprint
 local function move_inner_blueprint(event)
     local player = Player.from_event(event)
     
-    if not get_player_store(player):is_editing() then
+    if not is_editing(player) then
         player:sendmessage("Can't move selection, not currently editing.")
         return false
     end
     
-    if not get_player_store(player):has_selection() then
+    if not has_blueprint_selection(player) then
         player:sendmessage("Can't move selection, don't currently have anything selected.")
         return false
     end
@@ -64,7 +85,7 @@ Api.move_inner_blueprint = move_inner_blueprint
 local function stop_editing(event)
     local player = Player.from_event(event)
     
-    if get_player_store(player):is_editing() then
+    if is_editing(player) then
         Blueprint_Edit_Actions.player_stop_editing(player)
         return true
     end
--- lib/frontend/player/Player.lua
+++ lib/frontend/player/Player.lua
@@ -1,8 +1,22 @@
 local Object = require('lib.core.types.Object')
+local Table = require('lib.core.types.Table')
 
 local Player = Object.new_class()
+Player.type = "Player"
+
+function Player:is_player()
+    if(type(self) ~= "table")then
+        return false
+    end
+    if(type(self.index) ~= "number")then
+        return false
+    end
+    return true
+end
 
 local function from_event(event)
+    assert(type(event) == "table", "Event must be a table.")
+    assert(type(event.player_index) == "number", "Player index must be a number.")
     local new_object = {index = event.player_index}
     
     return Object.instantiate(new_object, Player)
@@ -10,6 +24,7 @@ end
 Player.from_event = from_event
 
 function Player:get_lua_player()
+    assert(Player.is_player(self), "Tried to get a non-player var " .. Table.to_string(self) .. " as player.")
     return game.players[self.index] -- patch job until I can figure out how to do . on instance for method including self
 end
 
@@ -25,17 +40,28 @@ function Player:get_open_gui_type()
     return self:get_lua_player().opened_gui_type
 end
 
+function Player:get_cursor_stack()
+    return self:get_lua_player().cursor_stack
+end
+
+function Player:get_selected()
+    return self:get_lua_player().selected
+end
+
+function Player:clean_cursor()
+    return self:get_lua_player().clean_cursor()
+end
+
+function Player:pipette_entity(entity)
+    return self:get_lua_player().pipette_entity(entity)
+end
+
 function Player:get_blueprint_from_hand()
     local stack = self:get_lua_player().cursor_stack
     if not stack or not stack.valid_for_read or stack.type ~= "blueprint" then
       return false
     end
     return stack -- TODO: where we want to transform stack to BP via copy
-end
-
-function Player:has_blueprint_in_hand()
-    if(self:get_blueprint_from_hand()) then return true end
-    return false
 end
 
 return Player
