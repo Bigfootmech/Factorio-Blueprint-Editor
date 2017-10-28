f0b012c62a0f6b10592b94318532986d1c4b4604
Bigfootmech <bigfootmech@gmail.com>
2017-10-29 00:16:18 +0100

    dev_qol_deployment ok, cleared that up. Just looks a bit weird

--- build_script_helpers/config_file_writer.lua
+++ build_script_helpers/config_file_writer.lua
@@ -1,6 +1,5 @@
 function write_config_file(filename, values_table)
-    print(filename)
-    local f = io.open ("./target/generated/locale/en/controls.cfg", "w")
+    local f = io.open (filename, "w")
     f:write("[controls]")
     for k,v in pairs(values_table)do
         f:write ("\n" .. tostring(k) .. "=" .. tostring(v))
