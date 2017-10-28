9069e67c905e0f5089c22ea346236bfaf7e39d16
Bigfootmech <bigfootmech@gmail.com>
2017-10-28 08:17:17 +0100

    dev_qol_deployment build.py works again. Tests now broken :P

--- build.py
+++ build.py
@@ -1,19 +1,24 @@
 import subprocess
 
 def generate_files():
-    
+    print("Generating files")
 
 def run_tests():
+    print("Running tests")
     result = subprocess.run(['lua', '-l', 'test.Suite_Test'], shell=True)
 
     print("Errors: " + str(result.returncode))
     
     return result.returncode
     
-def copy_files()
+def copy_files():
+    print("Copying files")
     
 
-def zip()
+def zip():
+    print("Creating zip")
     
+run_tests()
+
 
 input("Press Enter to close.")
