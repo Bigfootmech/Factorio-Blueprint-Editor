1c3d2ff7f4c07818cbd025b3a7cc6e6b6e485f2c
Bigfootmech <bigfootmech@gmail.com>
2017-10-29 21:27:53 +0000

    test fix

--- test/lib/logic/model/blueprint/Blueprint_Entity_Test.lua
+++ test/lib/logic/model/blueprint/Blueprint_Entity_Test.lua
@@ -1,11 +1,22 @@
 local lu = require('luaunit')
 local Blueprint_Entity = require('lib.logic.model.blueprint.Blueprint_Entity')
 
-TestCopy = {}
-    
-    function TestCopy:testCopyWorks()
+TestBlueprintEntity = {}
+
+    function TestBlueprintEntity:testFromTable()
         -- given
-        local blueprint_entity = {entity_number = 1, name = "dinosaur", position = {1, 1}}
+        local blueprint_entity_table = {entity_number = 1, name = "dinosaur", position = {1, 1}}
+        
+        -- when 
+        local result = Blueprint_Entity.from_table(blueprint_entity_table)
+        
+        -- then
+        lu.assertTrue(result)
+    end
+    
+    function TestBlueprintEntity:testCopyWorks()
+        -- given
+        local blueprint_entity = Blueprint_Entity.from_table({entity_number = 1, name = "dinosaur", position = {1, 1}})
         
         -- when 
         local result = Blueprint_Entity.copy(blueprint_entity)
