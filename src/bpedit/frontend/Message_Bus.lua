467544abfced13ff40843d93f7113252f91e4d58
bigfootmech <bigfootmech@gmail.com>
2017-11-01 08:41:34 +0000

    STYLE: changing style of message bus to what I'm settling on

--- src/bpedit/frontend/Message_Bus.lua
+++ src/bpedit/frontend/Message_Bus.lua
@@ -3,12 +3,11 @@ local Keybinds = require('bpedit.frontend.keybinds.Keybinds')
 
 local Message_Bus = {}
 
-local function init()
+function Message_Bus.init()
     for interface_function_name, event_name_table in pairs(Keybinds.get_interface_mapping()) do
         script.on_event(event_name_table, Api[interface_function_name])
     end
     script.on_event(defines.events.on_player_configured_blueprint, Api.stop_editing)
 end
-Message_Bus.init = init
 
 return Message_Bus
