bcec70267b4a93bbb2d90ec3bcd9ec8005f929ae
bigfootmech <bigfootmech@gmail.com>
2017-11-01 14:47:59 +0000

    BUGFIX: removing debug lines from live code. I really need to implement a logger.

--- src/bpedit/backend/storage/Global_Dao.lua
+++ src/bpedit/backend/storage/Global_Dao.lua
@@ -18,13 +18,11 @@ local function get_global_store()
 end
 
 local function init_player(global_store, player_id)
-    Player.from_index(player_id):sendmessage("Making new dao")
     global_store[player_id] = Player_Store_Dao.new() -- need some way of initializing per player, if we want to split out "global" to lib
 end
 
 function Global_Dao.get_player_store(player_id)
     local global_store = get_global_store()
-    game.players[player_id].print("getting player dao")
     if(not global_store[player_id])then 
         init_player(global_store, player_id) 
     end
