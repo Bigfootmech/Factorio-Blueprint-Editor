c0ef54a9f08a5665600b9640ca3b41b8e4e3689d
Bigfootmech <bigfootmech@gmail.com>
2017-11-03 22:08:48 +0000

    TOOLS: Added possibility to log to non-natural or ten base

--- src/lib/core/Math.lua
+++ src/lib/core/Math.lua
@@ -1,12 +1,28 @@
 local Math = {}
 
-local function round(num, numDecimalPlaces)
+function Math.round(num, numDecimalPlaces)
     assert(type(num) == "number", "Can only round numbers")
     if numDecimalPlaces ~= nil then assert(type(numDecimalPlaces) == "number", "Must use number if specifying decimal rounding") end
     local mult = 10^(numDecimalPlaces or 0)
     return math.floor(num * mult + 0.5) / mult
 end
 
-Math.round = round
+function Math.ln(num)
+    return math.log(num)
+end
+
+function Math.log10(num)
+    return math.log10(num)
+end
+
+function Math.log(num, base)
+    assert(type(num) == "number", "Can only log numbers")
+    if(base == nil)then
+        return math.log(num)
+    end
+    assert(type(base) == "number", "Can only with a base that's a number")
+    
+    return math.log(num)/math.log(base) -- arbitrary whether log or log10 used, don't know about performance.
+end
 
 return Math
