9893555bd134ab09d013b38f84c634248302767b
Bigfootmech <bigfootmech@gmail.com>
2017-11-03 23:05:50 +0000

    DEV_QOL: Added some limited support for greater than 26 registered actions.

--- src/lib/frontend/keybinds/Util.lua
+++ src/lib/frontend/keybinds/Util.lua
@@ -18,7 +18,7 @@ function Util.to_var_style(name)
     return string.gsub(string.lower(name), "%s+", var_separator)
 end
 
-function Util.to_keystroke_style(key) -- enum containing all "keystroke" characters?
+function Util.to_keystroke_style(key)
     return string.upper(key)
 end
 
@@ -26,12 +26,44 @@ local function event_style_concat(str1, str2)
     return str1..event_separator..str2
 end
 
-local function ordering_element(index) -- will break at 26, can be improved. floor(log25(total)+1)??? = number of characters required???. re-index every table? / force static calling?
-    return alphabet[index]
+local function reindex_for_new_char(list_of_ordered_elements)
+    local reindexed = {}
+    for _, ord_el in pairs(list_of_ordered_elements)do
+        table.insert(reindexed,alphabet[1] .. ord_el)
+    end
+    return reindexed
+end
+
+local function get_function_limit(characters_used) --  floor(log25(total)+1)??? = number of characters required??? (haven't checked)
+    return ((#alphabet-1) ^ characters_used) + 1
+end
+
+local function ordering_element(index, characters_used)
+    if(characters_used == nil)then
+        characters_used = 1
+    end
+    
+    if(index > get_function_limit(characters_used))then
+        error("Exceeded number of supported actions") --  add a in front of all the previously registered actions, and increase characters used
+    end
+    
+    if(characters_used == 0)then
+        return ""
+    end
+    
+    if(index <= #alphabet)then
+        return alphabet[index]
+    end
+    
+    local lowest_part_index = (index-1) % #alphabet + 1
+    
+    local shifted_index = math.floor((index - 1) / #alphabet) + 1
+    
+    return ordering_element(shifted_index, characters_used - 1) .. alphabet[lowest_part_index]
 end
 
 function Util.get_event_name(action_name, index)
-    return event_style_concat(ordering_element(index), to_event_style(action_name))
+    return event_style_concat(ordering_element(index, 2), to_event_style(action_name))
 end
 
 function Util.get_prototype_table(event_name, key_sequence)
