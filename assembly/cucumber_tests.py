c72d73ca00044bdbc95d27fb3e57313d6c4de60e
Bigfootmech <bigfootmech@gmail.com>
2017-11-05 17:45:50 +0000

    End_to_end_testing: console doesn't want to close :(

--- assembly/cucumber_tests.py
+++ assembly/cucumber_tests.py
@@ -1,15 +1,19 @@
 import os
 import subprocess
-import _thread as thread
 
 def launch_lua_cucumber():
-    subprocess.run(['cucumber-lua'], shell=True, stdout=subprocess.DEVNULL)
+    return subprocess.Popen(['cucumber-lua'], shell=True, stdout=subprocess.DEVNULL)
 
 def launch_cucumber():
-    subprocess.run(['cucumber'], shell=True)
+    return subprocess.run(['cucumber'], shell=True)
     
 def run_tests(it_folder, return_from_it_folder):
     os.chdir(it_folder)
-    t = thread.start_new_thread(launch_lua_cucumber, ())
+    print("Starting cucumber lua server")
+    p = launch_lua_cucumber()
+    print("Testing")
     result = launch_cucumber()
+    print("Stopping cucumber lua server")
+    p.terminate()
     os.chdir(return_from_it_folder)
+    return result
