4fd273dab7348b0822e587fdad255e623d96c947
Bigfootmech <bigfootmech@gmail.com>
2017-11-06 16:36:55 +0000

    lua_unit_integration: deciding whether to reimplement cucumber, or just get on with luaunit. Think I want luaunit

--- src/it/spec/bpedit/edit_or_reopen_blueprint_spec.lua
+++ src/it/spec/bpedit/edit_or_reopen_blueprint_spec.lua
@@ -1,3 +1,24 @@
 require('faketorio_busted')
 local lu = require('luaunit')
 
+EditOrReopenBlueprintTest = {}
+
+    function EditOrReopenBlueprintTest:showsErrorIfNothingEditingOrEditable()
+        Given("Player is editing " .. "nothing")
+        And("Player hand contains " .. "nothing")
+        And("Player mouseover selection contains " .. "nothing") 
+        
+        When("Player presses" .. "N") 
+        
+        Then("Player receives text " .. "Error")
+    end
+
+    function EditOrReopenBlueprintTest:editBlueprintFromHand()
+        Given("Player is editing " .. "nothing")
+        And("Player mouseover selection contains " .. "nothing") 
+        And("Player hand contains " .. "a blueprint")
+        
+        When("Player presses" .. "N") 
+        
+        Then("Player is now editing " .. "a blueprint")
+    end
