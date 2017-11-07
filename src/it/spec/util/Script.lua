2f26680b70d6cde1a29755d17d910dded98e3d2e
Bigfootmech <bigfootmech@gmail.com>
2017-11-07 18:10:26 +0000

    lua_unit_integration: the things you miss when you're tired.

--- src/it/spec/util/Script.lua
+++ src/it/spec/util/Script.lua
@@ -2,14 +2,13 @@ local global_hidden_event_store = {}
 
 function script.on_event(registering_events, function_to_trigger)
     if(type(registering_events) ~= "table")then
-        script.on_event({registering_events}, function_to_trigger)
+        return script.on_event({registering_events}, function_to_trigger)
     end
     global_hidden_event_store[function_to_trigger] = registering_events
 end
 
 function script.raise_event(hopefully_registered_event, event_happening)
     for function_to_trigger, registered_events_array in pairs(global_hidden_event_store)do
-    print("> " .. tostring(registered_events_array))
         for _, registered_event in ipairs(registered_events_array)do
             if(registered_event == hopefully_registered_event)then
                 return function_to_trigger(event_happening)
