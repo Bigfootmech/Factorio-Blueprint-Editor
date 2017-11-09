5e309b73c47a68ab0880af1cdf0885365088a103
Bigfootmech <bigfootmech@gmail.com>
2017-11-09 23:45:47 +0000

    Alignment: adding useful math functions

--- src/main/lua/lib/core/Math.lua
+++ src/main/lua/lib/core/Math.lua
@@ -1,10 +1,28 @@
 local Math = {}
 
+local small_epsilon = 0.00000001
+
 function Math.round(num, numDecimalPlaces)
     assert(type(num) == "number", "Can only round numbers")
     if numDecimalPlaces ~= nil then assert(type(numDecimalPlaces) == "number", "Must use number if specifying decimal rounding") end
     local mult = 10^(numDecimalPlaces or 0)
     return math.floor(num * mult + 0.5) / mult
+end
+
+function Math.half_floor(num)
+    return (math.floor(num * 2)) / 2
+end
+
+function Math.half_ceil(num)
+    return (math.ceil(num * 2)) / 2
+end
+
+function Math.is_whole(num)
+    assert(type(num) == "number", "Can only check wholeness of numbers")
+    
+    local rounded = Math.round(num)
+    
+    return math.abs(rounded - num) < small_epsilon
 end
 
 function Math.ln(num)
