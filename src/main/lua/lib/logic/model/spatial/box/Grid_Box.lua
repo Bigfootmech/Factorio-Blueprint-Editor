04f4fd1101c566cab9216f2739e7ed041994b645
Bigfootmech <bigfootmech@gmail.com>
2017-11-11 19:37:49 +0000

    better_anchor: adding grid and tile boxes, basically they're on, and perfectly off grid respectively

--- src/main/lua/lib/logic/model/spatial/box/Grid_Box.lua
+++ src/main/lua/lib/logic/model/spatial/box/Grid_Box.lua
@@ -2,20 +2,20 @@ local Object = require('lib.core.types.Object')
 local Whole_Box = require('lib.logic.model.spatial.box.Whole_Box')
 local Position = require('lib.logic.model.spatial.Position')
 
-local classname = "Tile_Box"
+local classname = "Grid_Box"
 local superclass = Whole_Box
 
 local Self = Object.extends(superclass, classname)
 
-function Self:is_tile_box()
+function Self:is_grid_box()
     if(not superclass.is_bounding_box(self))then
         return false
     end
     
-    if(not Position.is_perfectly_off_grid(superclass.get_left_top(self)))then
+    if(not Position.is_on_grid(superclass.get_left_top(self)))then
         return false
     end
-    if(not Position.is_perfectly_off_grid(superclass.get_right_bottom(self)))then
+    if(not Position.is_on_grid(superclass.get_right_bottom(self)))then
         return false
     end
     
@@ -26,8 +26,8 @@ function Self.new(left_top, right_bottom)
     assert(Position.is_position(left_top), "left_top was not a valid position.")
     assert(Position.is_position(right_bottom), "right_bottom was not a valid position.")
     
-    assert(left_top:is_perfectly_off_grid(), "invalid position for" .. classname)
-    assert(right_bottom:is_perfectly_off_grid(), "invalid position for" .. classname)
+    assert(left_top:is_on_grid(), "invalid position for" .. classname)
+    assert(right_bottom:is_on_grid(), "invalid position for" .. classname)
     
     local newObject = superclass.new(left_top, right_bottom)
     
@@ -35,15 +35,11 @@ function Self.new(left_top, right_bottom)
 end
 
 function Self.from_bounding_box_inner(bounding_box)
-    return Self.new(bounding_box:get_left_top():move_perfectly_off_grid_ceil(), bounding_box:get_right_bottom():move_perfectly_off_grid_floor())
+    return Self.new(bounding_box:get_left_top():move_to_grid_ceil(), bounding_box:get_right_bottom():move_to_grid_floor())
 end
 
 function Self.from_bounding_box_outer(bounding_box)
-    return Self.new(bounding_box:get_left_top():move_perfectly_off_grid_floor(), bounding_box:get_right_bottom():move_perfectly_off_grid_ceil())
-end
-
-function Self.from_collision_box(bounding_box)
-    Self.from_bounding_box_outer(bounding_box)
+    return Self.new(bounding_box:get_left_top():move_to_grid_floor(), bounding_box:get_right_bottom():move_to_grid_ceil())
 end
 
 return Self
---
+++ src/main/lua/lib/logic/model/spatial/box/Whole_Box.lua
@@ -0,0 +1,34 @@
+local Object = require('lib.core.types.Object')
+local Bounding_Box = require('lib.logic.model.spatial.box.Bounding_Box')
+local Math = require('lib.core.Math')
+
+local classname = "Tile_Box"
+local superclass = Bounding_Box
+
+local Self = Object.extends(superclass, classname)
+
+function Self:get_x_centre()
+    return superclass.get_x_centre(self):move_to_grid_ceil()
+end
+
+function Self:get_y_centre()
+    return superclass.get_y_centre(self):move_to_grid_ceil()
+end
+
+function Self:get_width()
+    return math.ceil(superclass.get_width(self))
+end
+
+function Self:get_height()
+    return math.ceil(superclass.get_height(self))
+end
+
+function Self:is_width_even()
+    return Math.is_even(self:get_width())
+end
+
+function Self:is_height_even()
+    return Math.is_even(self:get_height())
+end
+
+return Self
