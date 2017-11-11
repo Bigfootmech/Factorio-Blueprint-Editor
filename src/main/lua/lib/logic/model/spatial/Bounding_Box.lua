b24fbc15b36d1eb3af90aa55864a9b0aa52d0aed
Bigfootmech <bigfootmech@gmail.com>
2017-11-11 18:14:36 +0000

    Code Quality: Changing Bounding_Box.lua to new class style

--- src/main/lua/lib/logic/model/spatial/Bounding_Box.lua
+++ src/main/lua/lib/logic/model/spatial/Bounding_Box.lua
@@ -22,182 +22,179 @@ local Math = require('lib.core.Math')
 local Position = require('lib.logic.model.spatial.Position')
 local Vector = require('lib.logic.model.spatial.Vector')
 
-local Bounding_Box = Object.new_class()
-Bounding_Box.type = "Bounding_Box"
+local classname = "Bounding_Box"
+local Self = Object.new_class(classname)
 
-function Bounding_Box:get_left_top_explicit()
+function Self:get_left_top_explicit()
     assert(type(self) == "table", "Can not get-left_top of a non-table.")
     return self.left_top
 end
-function Bounding_Box:get_right_bottom_explicit()
+function Self:get_right_bottom_explicit()
     assert(type(self) == "table", "Can not get-bottom_right of a non-table.")
     return self.right_bottom
 end
-function Bounding_Box:get_left_top_shorthand()
+function Self:get_left_top_shorthand()
     assert(type(self) == "table", "Can not get-left_top of a non-table.")
     return self[1]
 end
-function Bounding_Box:get_right_bottom_shorthand()
+function Self:get_right_bottom_shorthand()
     assert(type(self) == "table", "Can not get-bottom_right of a non-table.")
     return self[2]
 end
 
-function Bounding_Box:is_explicit_bounding_box()
+function Self:is_explicit_bounding_box()
     if(type(self) ~= "table")then
         return false
     end
-    if(not Position.is_position(Bounding_Box.get_left_top_explicit(self)))then
+    if(not Position.is_position(Self.get_left_top_explicit(self)))then
         return false
     end
-    if(not Position.is_position(Bounding_Box.get_right_bottom_explicit(self)))then
+    if(not Position.is_position(Self.get_right_bottom_explicit(self)))then
         return false
     end
     return true
 end
 
-function Bounding_Box:is_shorthand_bounding_box()
+function Self:is_shorthand_bounding_box()
     if(type(self) ~= "table")then
         return false
     end
-    if(not Position.is_position(Bounding_Box.get_left_top_shorthand(self)))then
+    if(not Position.is_position(Self.get_left_top_shorthand(self)))then
         return false
     end
-    if(not Position.is_position(Bounding_Box.get_right_bottom_shorthand(self)))then
+    if(not Position.is_position(Self.get_right_bottom_shorthand(self)))then
         return false
     end
     return true
 end
 
-function Bounding_Box:is_bounding_box()
-    if(type(self) ~= "table")then
-        return false
-    end
-    if(Bounding_Box.type == "Bounding_Box")then
+function Self:is_bounding_box()
+    if(Object.is_type(self, classname))then
         return true
     end
-    if(Bounding_Box.is_explicit_bounding_box(self))then
+    if(Self.is_explicit_bounding_box(self))then
         return true
     end
-    if(Bounding_Box.is_shorthand_bounding_box(self))then
+    if(Self.is_shorthand_bounding_box(self))then
         return true
     end
     return false
 end
 
-function Bounding_Box:get_left_top()
-    assert(Bounding_Box.is_bounding_box(self), "Can not use this method on non-bounding_box.")
-    if(Bounding_Box.is_explicit_bounding_box(self))then
-        return Bounding_Box.get_left_top_explicit(self)
+function Self:get_left_top()
+    assert(Self.is_bounding_box(self), "Can not use this method on non-bounding_box.")
+    if(Self.is_explicit_bounding_box(self))then
+        return Self.get_left_top_explicit(self)
     end
-    if(Bounding_Box.is_shorthand_bounding_box(self))then
-        return Bounding_Box.get_left_top_shorthand(self)
+    if(Self.is_shorthand_bounding_box(self))then
+        return Self.get_left_top_shorthand(self)
     end
     -- error
 end
-function Bounding_Box:get_right_bottom()
-    assert(Bounding_Box.is_bounding_box(self), "Can not use this method on non-bounding_box.")
-    if(Bounding_Box.is_explicit_bounding_box(self))then
-        return Bounding_Box.get_right_bottom_explicit(self)
+function Self:get_right_bottom()
+    assert(Self.is_bounding_box(self), "Can not use this method on non-bounding_box.")
+    if(Self.is_explicit_bounding_box(self))then
+        return Self.get_right_bottom_explicit(self)
     end
-    if(Bounding_Box.is_shorthand_bounding_box(self))then
-        return Bounding_Box.get_right_bottom_shorthand(self)
+    if(Self.is_shorthand_bounding_box(self))then
+        return Self.get_right_bottom_shorthand(self)
     end
     -- error
 end
 
-function Bounding_Box:get_right()
+function Self:get_right()
     return self:get_right_bottom():get_x()
 end
 
-function Bounding_Box:get_bottom()
+function Self:get_bottom()
     return self:get_right_bottom():get_y()
 end
 
-function Bounding_Box:get_left()
+function Self:get_left()
     return self:get_left_top():get_x()
 end
 
-function Bounding_Box:get_top()
+function Self:get_top()
     return self:get_left_top():get_y()
 end
 
-function Bounding_Box:get_right_top()
+function Self:get_right_top()
     return Position.new(self:get_right(), self:get_top())
 end
 
-function Bounding_Box:get_left_bottom()
+function Self:get_left_bottom()
     return Position.new(self:get_left(), self:get_bottom())
 end
 
-function Bounding_Box:get_x_centre()
+function Self:get_x_centre()
     return (self:get_left_top():get_x() + self:get_right_bottom():get_x())/2
 end
 
-function Bounding_Box:get_y_centre()
+function Self:get_y_centre()
     return (self:get_left_top():get_y() + self:get_right_bottom():get_y())/2
 end
 
-function Bounding_Box:get_centre_top()
+function Self:get_centre_top()
     return Position.new(self:get_x_centre(), self:get_top())
 end
 
-function Bounding_Box:get_centre_bottom()
+function Self:get_centre_bottom()
     return Position.new(self:get_x_centre(), self:get_bottom())
 end
 
-function Bounding_Box:get_right_centre()
+function Self:get_right_centre()
     return Position.new(self:get_right(), self:get_y_centre())
 end
 
-function Bounding_Box:get_left_centre()
+function Self:get_left_centre()
     return Position.new(self:get_left(), self:get_y_centre())
 end
 
-function Bounding_Box:get_centre_centre()
+function Self:get_centre_centre()
     return Position.new(self:get_x_centre(), self:get_y_centre())
 end
 
-function Bounding_Box:get_point(point_name)
+function Self:get_point(point_name)
     local method_name = "get_" .. point_name
-    return Bounding_Box[method_name](self)
+    return Self[method_name](self)
 end
 
-function Bounding_Box:get_width()
+function Self:get_width()
     return self:get_right() - self:get_left()
 end
 
-function Bounding_Box:get_height()
+function Self:get_height()
     return self:get_bottom() - self:get_top()
 end
 
-function Bounding_Box.new(left_top, right_bottom)
+function Self.new(left_top, right_bottom)
     assert(Position.is_position(left_top), "left_top was not a valid position.")
     assert(Position.is_position(right_bottom), "right_bottom was not a valid position.")
     
     local newObject = {left_top = left_top, right_bottom = right_bottom}
     
-    return Object.instantiate(newObject, Bounding_Box)
+    return Object.instantiate(newObject, Self)
 end
 
-function Bounding_Box.from_table(bounding_box)
-    assert(Bounding_Box.is_bounding_box(bounding_box), "Can't convert " .. tostring(bounding_box) .. " to Bounding Box.")
+function Self.from_table(bounding_box)
+    assert(Self.is_bounding_box(bounding_box), "Can't convert " .. tostring(bounding_box) .. " to Bounding Box.")
     
-    local left_top = Position.from_table(Bounding_Box.get_left_top(bounding_box))
-    local right_bottom = Position.from_table(Bounding_Box.get_right_bottom(bounding_box))
+    local left_top = Position.from_table(Self.get_left_top(bounding_box))
+    local right_bottom = Position.from_table(Self.get_right_bottom(bounding_box))
     
-    return Bounding_Box.new(left_top, right_bottom)
+    return Self.new(left_top, right_bottom)
 end
 
-function Bounding_Box.from_position(position)
-    return Bounding_Box.new(position, position)
+function Self.from_position(position)
+    return Self.new(position, position)
 end
 
-function Bounding_Box:copy()
+function Self:copy()
     assert(self:is_bounding_box(), "Tried to Bounding_Box.copy a non-Bounding_Box object")
-    return Bounding_Box.new(self.get_left_top():copy(), self.get_right_bottom():copy())
+    return Self.new(self.get_left_top():copy(), self.get_right_bottom():copy())
 end
 
-function Bounding_Box:is_position_outside_left_top(position)
+function Self:is_position_outside_left_top(position)
     local left_top = self:get_left_top()
     
     if(position:get_x() < left_top:get_x())then
@@ -209,7 +206,7 @@ function Bounding_Box:is_position_outside_left_top(position)
     return false
 end
 
-function Bounding_Box:is_position_outside_right_bottom(position)
+function Self:is_position_outside_right_bottom(position)
     local right_bottom = self:get_right_bottom()
     
     if(position:get_x() > right_bottom:get_x())then
@@ -221,7 +218,7 @@ function Bounding_Box:is_position_outside_right_bottom(position)
     return false
 end
 
-function Bounding_Box:contains(position)
+function Self:contains(position)
     if(self:is_position_outside_left_top(position))then
         return false
     end
@@ -231,7 +228,7 @@ function Bounding_Box:contains(position)
     return true
 end
 
-function Bounding_Box:include_position(position)
+function Self:include_position(position)
     if(self:contains(position))then
         return self
     end
@@ -251,4 +248,4 @@ function Bounding_Box:include_position(position)
     return self
 end
 
-return Bounding_Box
+return Self
