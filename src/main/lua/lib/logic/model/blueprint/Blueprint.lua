40a1a8013aa053d3e186d4ba6514f8ecd4d0ec95
Bigfootmech <bigfootmech@gmail.com>
2017-12-06 09:45:13 +0000

    Editing_none: Game quibbles about empty blueprints.

--- src/main/lua/lib/logic/model/blueprint/Blueprint.lua
+++ src/main/lua/lib/logic/model/blueprint/Blueprint.lua
@@ -91,7 +91,7 @@ function Blueprint.from_table(table)
 end
 
 function Blueprint.empty()
-    return Blueprint.from_table({blueprint_entities={}, blueprint_tiles={}, label="", blueprint_icons={}})
+    return Blueprint.from_table({blueprint_entities={}, blueprint_tiles={}, label=""})
 end
 
 function Blueprint.from_lua_blueprint(lua_blueprint)
@@ -112,7 +112,9 @@ function Blueprint:dump_to_lua_blueprint(lua_blueprint)
     lua_blueprint.set_blueprint_entities(self.blueprint_entities)
     lua_blueprint.set_blueprint_tiles(self.blueprint_tiles)
     lua_blueprint.label = self.label or "" -- sends nil, but doesn't accept nil
-    lua_blueprint.blueprint_icons = self.blueprint_icons
+    if(self.blueprint_icons)then
+        lua_blueprint.blueprint_icons = self.blueprint_icons
+    end
 end
 
 function Blueprint:add_entity(blueprint_entity)
